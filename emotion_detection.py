239a6b83137a90b160238f45b4e3f96b709a24ac
jasonclarkdevuk <jasonclarkdev@gmail.com>
2026-05-10 10:21:50 -0400

    Convert response to JSON and extract emotion data. Add logic to add to data response what is the dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,9 +7,11 @@ import requests
 def emotion_detector(text_to_analyze):
     '''
     A function that gathers emotion data.
-    It provides the tone label and the score.
+    Provides scores for anger, disgust, fear, joy and sadness.
     Provides errors if there is invalid input.
     '''
+    emotion_data = {}
+
     url = "https://sn-watson-emotion.labs.skills.network/v1"
     url_2 = "/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -18,4 +20,25 @@ def emotion_detector(text_to_analyze):
     # HTTP Post request to get emotion data
     response = requests.post(url + url_2, json=input_json, headers=headers)
 
-    return response.text
+    # Convert response to JSON dictionary
+    response_data = json.loads(response.text)
+
+    # Return emotion data
+    emotion_data["anger"] = response_data["emotionPredictions"][0]["emotion"]["anger"]
+    emotion_data["disgust"] = response_data["emotionPredictions"][0]["emotion"]["disgust"]
+    emotion_data["fear"] = response_data["emotionPredictions"][0]["emotion"]["fear"]
+    emotion_data["joy"] = response_data["emotionPredictions"][0]["emotion"]["joy"]
+    emotion_data["sadness"] = response_data["emotionPredictions"][0]["emotion"]["sadness"]
+
+    # Figure out dominant emotion
+    dominant_emotion = "anger"
+
+    emotion_checks = ["disgust", "fear", "joy", "sadness"]
+
+    for check in emotion_checks:
+        if (emotion_data[check] > emotion_data[dominant_emotion]):
+            dominant_emotion = check
+    
+    emotion_data["dominant_emotion"] = dominant_emotion
+
+    return emotion_data
